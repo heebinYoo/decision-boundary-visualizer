574cccab5d07c1cd8b85ce46f8049fd8a3e39002
heebin <1999bin@gmail.com>
2022-01-11 16:27:02 +0900

    done 3d visualization code

--- mnist-train.py
+++ mnist-train.py
@@ -3,8 +3,6 @@ import os
 
 from model import ConfidenceControl
 
-
-
 import argparse
 
 import numpy as np
@@ -75,7 +73,7 @@ def parseArgument():
 
     parser.add_argument('--batch_size', default=512, type=int, help='train batch size')
     parser.add_argument('--num_sample', default=4, type=int, help='samples within each class')
-    parser.add_argument('--feature_dim', default=2, type=int, help='feature dim')
+    parser.add_argument('--feature_dim', default=3, type=int, help='feature dim')
     parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
     parser.add_argument('--lr_gamma', default=0.1, type=float, help='learning rate scheduler gamma')
     parser.add_argument('--num_epochs', default=3, type=int, help='train epoch number')
@@ -85,9 +83,7 @@ def parseArgument():
     return opt.data_path, opt.data_name, opt.crop_type, opt.batch_size, opt.num_sample, opt.feature_dim, opt.lr, opt.lr_gamma, opt.num_epochs
 
 
-
-def loadData(batch_size, num_sample, seen=(0,1), unseen=(4,5)):
-
+def loadData(batch_size, num_sample, seen=(0, 1), unseen=(4, 5)):
     mnist_transform = transforms.Compose([
         transforms.Resize((256, 256)),
         transforms.RandomCrop(224),
@@ -97,35 +93,31 @@ def loadData(batch_size, num_sample, seen=(0,1), unseen=(4,5)):
         transforms.Normalize((0.5,), (1.0,))
     ])
     train_data_set = MNIST(root='/data/hbyoo',  # 다운로드 경로 지정
-                               train=True,  # True를 지정하면 훈련 데이터로 다운로드
-                               transform=mnist_transform,  # 텐서로 변환
-                               download=True)
+                           train=True,  # True를 지정하면 훈련 데이터로 다운로드
+                           transform=mnist_transform,  # 텐서로 변환
+                           download=True)
 
     seen_idx = torch.cat((
-          torch.nonzero(train_data_set.targets == seen[0]).reshape(-1),
-          torch.nonzero(train_data_set.targets == seen[1]).reshape(-1)
+        torch.nonzero(train_data_set.targets == seen[0]).reshape(-1),
+        torch.nonzero(train_data_set.targets == seen[1]).reshape(-1),
+        torch.nonzero(train_data_set.targets == seen[2]).reshape(-1),
+        torch.nonzero(train_data_set.targets == seen[3]).reshape(-1)
     )).reshape(-1)
 
-
     unseen_idx = torch.cat((
-          torch.nonzero(train_data_set.targets == unseen[0]).reshape(-1),
-          torch.nonzero(train_data_set.targets == unseen[1]).reshape(-1)
+        torch.nonzero(train_data_set.targets == unseen[0]).reshape(-1),
+        torch.nonzero(train_data_set.targets == unseen[1]).reshape(-1)
     )).reshape(-1)
 
     seen_data = Subset(train_data_set, seen_idx)
     seen_label = train_data_set.targets[seen_idx]
     unseen_data = Subset(train_data_set, unseen_idx)
 
-
     train_data_loader = DataLoader(seen_data, batch_size, shuffle=True, num_workers=4)
     test_data_loader = DataLoader(unseen_data, batch_size, shuffle=False,
-                                   num_workers=4)
-
+                                  num_workers=4)
 
     return train_data_loader, test_data_loader, 10
-
-
-
 
 
 def setModel(device, feature_dim, number_of_class, model_type):
@@ -190,10 +182,6 @@ def get_embed(net, device, dataloader, feature_dim):
     return embedding_list, label_list
 
 
-
-
-
-
 def main(arg_data_name=None):
     device, device_count = setCuda()
     setSeed()
@@ -201,7 +189,7 @@ def main(arg_data_name=None):
     if arg_data_name is not None:
         data_name = arg_data_name
 
-    seen = (0, 1)
+    seen = (0, 1, 2, 3)
     unseen = (4, 5)
     train_data_loader, test_data_loader, number_of_class = loadData(batch_size, num_sample, seen, unseen)
     model = setModel(device, feature_dim, number_of_class,
@@ -211,34 +199,82 @@ def main(arg_data_name=None):
     lr_scheduler = StepLR(optimizer, step_size=15, gamma=lr_gamma)
     loss_criterion = nn.CrossEntropyLoss()
 
+    model.eval()
+    train_embedding_list, train_label_list = get_embed(model, device, train_data_loader, feature_dim)
+    test_embedding_list, test_label_list = get_embed(model, device, test_data_loader, feature_dim)
+    s0 = torch.from_numpy(
+        train_embedding_list[np.where(train_label_list == seen[0]), :].reshape(-1, 3).astype("float")).unsqueeze(0)
+    s1 = torch.from_numpy(
+        train_embedding_list[np.where(train_label_list == seen[1]), :].reshape(-1, 3).astype("float")).unsqueeze(0)
+    s2 = torch.from_numpy(
+        train_embedding_list[np.where(train_label_list == seen[2]), :].reshape(-1, 3).astype("float")).unsqueeze(0)
+    s3 = torch.from_numpy(
+        train_embedding_list[np.where(train_label_list == seen[3]), :].reshape(-1, 3).astype("float")).unsqueeze(0)
+    u0 = torch.from_numpy(
+        test_embedding_list[np.where(test_label_list == unseen[0]), :].reshape(-1, 3).astype("float")).unsqueeze(0)
+    u1 = torch.from_numpy(
+        test_embedding_list[np.where(test_label_list == unseen[1]), :].reshape(-1, 3).astype("float")).unsqueeze(0)
+
+    label = np.concatenate(
+        (np.array(s0.shape[1] * [0]).reshape(-1), np.array(s1.shape[1] * [1]).reshape(-1),
+         np.array(s2.shape[1] * [2]).reshape(-1), np.array(s3.shape[1] * [3]).reshape(-1),
+         np.array(u0.shape[1] * [4]).reshape(-1), np.array(u1.shape[1] * [5]).reshape(-1)))
+    label = torch.as_tensor(label).unsqueeze(0)
+
+    writer.add_embedding(torch.cat((s0, s1, s2, u0, u1), dim=1).resg,
+                         metadata=label)
+
+
+
+
+
+    # fig = plt.figure(figsize=(10, 10))
+    # ax = fig.add_subplot(111, projection='3d')
+    #
+    # ax.scatter(train_embedding_list[np.where(train_label_list == seen[0]), 0],
+    #            train_embedding_list[np.where(train_label_list == seen[0]), 1],
+    #            train_embedding_list[np.where(train_label_list == seen[0]), 2],
+    #            c='r', alpha=0.1, s=100,
+    #            label='seen1')
+    # ax.scatter(train_embedding_list[np.where(train_label_list == seen[1]), 0],
+    #            train_embedding_list[np.where(train_label_list == seen[1]), 1],
+    #            train_embedding_list[np.where(train_label_list == seen[1]), 2],
+    #            c='b', alpha=0.1, s=100,
+    #            label='seen2')
+    # ax.scatter(train_embedding_list[np.where(train_label_list == seen[2]), 0],
+    #            train_embedding_list[np.where(train_label_list == seen[2]), 1],
+    #            train_embedding_list[np.where(train_label_list == seen[2]), 2],
+    #            c='#41f1f1', alpha=0.1, s=100,
+    #            label='seen3')
+    # ax.scatter(test_embedding_list[np.where(test_label_list == unseen[0]), 0],
+    #            test_embedding_list[np.where(test_label_list == unseen[0]), 1],
+    #            test_embedding_list[np.where(test_label_list == unseen[0]), 2],
+    #            c='g', alpha=0.5, s=5,
+    #            label='unseen1')
+    # ax.scatter(test_embedding_list[np.where(test_label_list == unseen[1]), 0],
+    #            test_embedding_list[np.where(test_label_list == unseen[1]), 1],
+    #            test_embedding_list[np.where(test_label_list == unseen[1]), 2],
+    #            c='y', alpha=0.5, s=5,
+    #            label='unseen2')
+    # plt.legend()
+    #
+    # writer.add_figure('mnist-embedding-space', fig,  0)
+
     for epoch in range(num_epochs):
 
         model.train()
         average_loss = train(model, train_data_loader, device, loss_criterion,
                              optimizer_init if epoch == 1 else optimizer)
         writer.add_scalar("Loss/average_train_loss", average_loss, epoch)
+
         if epoch >= 2:
             lr_scheduler.step()
-    model.eval()
-
-    train_embedding_list, train_label_list = get_embed(model, device, train_data_loader, feature_dim)
-    test_embedding_list, test_label_list = get_embed(model, device, test_data_loader, feature_dim)
 
 
-    fig = plt.figure(figsize=(10, 10))
-    plt.scatter(train_embedding_list[np.where(train_label_list == seen[0]), 0], train_embedding_list[np.where(train_label_list == seen[0]), 1], c='r', alpha=0.1, s=100, label='seen1')
-    plt.scatter(train_embedding_list[np.where(train_label_list == seen[1]), 0], train_embedding_list[np.where(train_label_list == seen[1]), 1], c='b', alpha=0.1, s=100, label='seen2')
-    plt.scatter(test_embedding_list[np.where(test_label_list == unseen[0]), 0], test_embedding_list[np.where(test_label_list == unseen[0]), 1], c='g', alpha=0.5, s=5, label='unseen1')
-    plt.scatter(test_embedding_list[np.where(test_label_list == unseen[1]), 0], test_embedding_list[np.where(test_label_list == unseen[1]), 1], c='y', alpha=0.5, s=5, label='unseen2')
-    plt.legend()
-
-    #plt.show()
-
-    writer.add_figure('mnist-embedding-space', fig, num_epochs)
 
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"  # "0""None"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"  # "0""None"
 
 if __name__ == '__main__':
     # for dataset in ['car', 'cub', 'sop', 'isc', 'mnist']:
